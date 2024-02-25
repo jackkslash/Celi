d3a621746f3a7eb427d7fdbbd425ae7ec092fc16
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-02-25 00:52:40 +0000

    endpoint to clone repos into local output folder with UUIDs

--- celi-upload-service/src/index.ts
+++ celi-upload-service/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World")
+import express from "express";
+import cors from "cors";
+import { randomUUID } from "crypto";
+import simpleGit from "simple-git";
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.post("/deploy", async (req, res) => {
+    const repoUrl = req.body.repoUrl;
+    const UUID = randomUUID();
+    await simpleGit().clone(repoUrl, `output/${UUID}`)
+    res.json({
+        UUID: UUID
+    })
+});
+
+app.listen(3000);
