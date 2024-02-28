e6f664e6c5c13e214db0ad596856441a927e8a3a
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-02-28 01:28:51 +0000

    deploy service to pop the UUID once  pushed to the build queue

--- celi-deploy-service/src/index.ts
+++ celi-deploy-service/src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+import { commandOptions, createClient } from "redis";
+
+const subscriber = createClient();
+subscriber.connect();
+
+
+(async function () {
+    while (1) {
+        const res = await subscriber.brPop(
+            commandOptions({ isolated: true }),
+            'build-queue',
+            0
+        )
+        const id = res?.element
+        console.log(res)
+        console.log(id)
+    }
+})();
+
