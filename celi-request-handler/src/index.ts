f3107838c17151f70bca7548afd3e42ae3a5776d
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-03-05 19:41:23 +0000

    read subdomain id and test instructions for new domains

--- celi-request-handler/src/index.ts
+++ celi-request-handler/src/index.ts
@@ -2,4 +2,14 @@ import express from "express";
 
 const app = express();
 
+//sudo nano /etc/hosts
+//local.localhost
+
+app.get("/*", async (req, res) => {
+    const host = req.hostname;
+    console.log(host)
+    const id = host.split(".")[0];
+    console.log(id)
+})
+
 app.listen(3000)
