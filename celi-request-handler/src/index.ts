0e2aed192cf5ca50fc3976d97f10e5efa5597cdc
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-03-06 22:59:32 +0000

    reads subdomain, matches with bucket file path and serves matching files

--- celi-request-handler/src/index.ts
+++ celi-request-handler/src/index.ts
@@ -1,15 +1,30 @@
 import express from "express";
+import { S3 } from "aws-sdk";
+import 'dotenv/config'
 
 const app = express();
 
+const s3 = new S3({
+    accessKeyId: process.env.ACCESSKEYID,
+    secretAccessKey: process.env.SECRECTACCESSKEY,
+    endpoint: process.env.ENDPOINT
+})
 //sudo nano /etc/hosts
-//local.localhost
 
 app.get("/*", async (req, res) => {
     const host = req.hostname;
-    console.log(host)
     const id = host.split(".")[0];
+    const filePath = req.path
     console.log(id)
+    const contents = await s3.getObject({
+        Bucket: "celi",
+        Key: `dist/${id}${filePath}`
+    }).promise();
+
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    res.set("Content-Type", type);
+    res.send(contents.Body)
+
 })
 
-app.listen(3000)
+app.listen(3001)
